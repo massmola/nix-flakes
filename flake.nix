f7928440f9534ae6245dd8ce987306504137257d
MassimiIano <massimiliano.mola.bzs@gmail.com>
2025-03-12 11:25:16 +0100

    added flake with jupiter notebook shell

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Jupyter Notebook environment for NixOS";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.python3                     # Provides Python
+          pkgs.python3Packages.jupyter     # Installs the jupyter command
+          pkgs.python3Packages.notebook    # Installs the notebook package
+        ];
+      };
+    };
+}
