0e97a68a7a59e52040261274cf4782a2b958d894
MassimiIano <massimiliano.mola.bzs@gmail.com>
2025-03-12 13:43:26 +0100

    made node shell work

--- flake.nix
+++ flake.nix
@@ -1,58 +1,60 @@
 {
-  description = "Jupyter Notebook environment for NixOS";
+  description = "Multiple dev shells example";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.defaultpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nodejs16pkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
   outputs =
-    { self, nixpkgs, ... }:
+    {
+      self,
+      defaultpkgs,
+      nodejs16pkgs,
+      ...
+    }:
     let
       system = "x86_64-linux";
-      pkgs = (
-        import nixpkgs {
-          inherit system;
-          config = {
-            permittedInsecurePackages = [
-              "nodejs-16.20.2"
-            ];
-          };
-        }
-      );
+
+      pkgs = import defaultpkgs {
+        inherit system;
+      };
+
+      nodePkgs = import nodejs16pkgs {
+        inherit system;
+        config = {
+          permittedInsecurePackages = [ "nodejs-16.20.2" ];
+        };
+      };
     in
     {
       devShells.${system} = {
         jupiter = pkgs.mkShell {
           buildInputs = [
-            pkgs.python3 # Provides Python
-            pkgs.python3Packages.jupyter # Installs the jupyter command
-            pkgs.python3Packages.notebook # Installs the notebook package
+            pkgs.python3
+            pkgs.python3Packages.jupyter
+            pkgs.python3Packages.notebook
           ];
-
           shellHook = ''
-            echo "Welcome to the Jupyter Notebook development environment!"
+            echo "Welcome to the Jupyter Notebook environment!"
           '';
         };
 
-        node16 = pkgs.mkShell {
-          permittedInsecurePackages = [ "nodejs-16.20.2" ];
+        node = pkgs.mkShell {
           buildInputs = [
-            pkgs.nodejs_16
+            nodePkgs.nodejs_16
           ];
-
           shellHook = ''
-            echo "Welcome to the Node.js 16 development environment!"
+            echo "Welcome to the Node.js development environment!"
           '';
         };
 
-        dotnet8 = pkgs.mkShell {
+        other = pkgs.mkShell {
           buildInputs = [
-            pkgs.dotnet-sdk_8 # .NET 8 SDK
+            pkgs.hello
           ];
-
           shellHook = ''
-            echo "Welcome to the .NET 8 development environment!"
+            echo "Welcome to the Hello world dev shell!"
           '';
         };
-
       };
     };
 }
